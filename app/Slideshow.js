0d4d28788095e221cbdf3e70e8241a1f5f566316
banhducnonggg <banhducnonggg@gmail.com>
2023-10-07 22:54:34 -0600

    slideshow commit

--- app/Slideshow.js
+++ app/Slideshow.js
@@ -9,6 +9,7 @@ const slides = [
 
 const Slideshow = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [isPopupOpen, setPopupOpen] = useState(false);
 
   const nextSlide = () => {
     setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
@@ -22,44 +23,79 @@ const Slideshow = () => {
     setCurrentSlide(slideIndex);
   };
 
+  const togglePopup = () => {
+    setPopupOpen(!isPopupOpen);
+  };
+
   return (
     <div className="slider-container select-none">
       <div>
-      {slides.map((slide, index) => (
-        <div className="flex items-center justify-center h-24 pageRedirect transition-opacity duration-20000">
-          <button className="ontop text-5xl font-titleFont text-lrColor">
-            <Link href='/'>
-              {slides[currentSlide].label}
-            </Link>
-          </button>
-        </div>
-      ))}
-      </div>
-        <div className="flex dot-container">
-          {slides.map((slide, index) => (
+        {slides.map((slide, index) => (
+          <div
+            className="flex items-center justify-center h-24 pageRedirect transition-opacity duration-20000"
+            key={slide.id}
+          >
             <button
-              key={slide.id}
-              className={`ontop dot h-3 w-3 rounded-full ${
-                index === currentSlide ? 'bg-lrColor drop-shadow-outGlow' : 'bg-lColor'
-              }`}
-              onClick={() => goToSlide(index)}
-            />
-          ))}
-        </div>
-      <div className="flex mt-4">
-        <button
-          className="prev ontop text-lColor font-navFont hover:text-lrColor"
-          onClick={prevSlide}
-        >
-          PREV
-        </button>
-        <button
-          className="next ontop text-lColor font-navFont hover:text-lrColor"
-          onClick={nextSlide}
-        >
-          NEXT
-        </button>
+              className="ontop text-5xl font-titleFont text-lrColor"
+              onClick={togglePopup} // Add click event to open the popup
+            >
+              {isPopupOpen ? null : slides[currentSlide].label}
+            </button>
+          </div>
+        ))}
       </div>
+      <div className="flex dot-container">
+        {slides.map((slide, index) => (
+          <button
+            key={slide.id}
+            className={`ontop dot h-3 w-3 rounded-full ${
+              index === currentSlide ? 'bg-lrColor drop-shadow-outGlow' : 'bg-lColor'
+            }`}
+            onClick={() => goToSlide(index)}
+          />
+        ))}
+      </div>
+      <div className="flex mt-4">
+        {isPopupOpen ? null : (
+          <>
+          
+            <button
+              className="prev ontop text-lColor font-navFont hover:text-lrColor"
+              onClick={prevSlide}
+            >
+              PREV
+            </button>
+            <button
+              className="next ontop text-lColor font-navFont hover:text-lrColor"
+              onClick={nextSlide}
+            >
+              NEXT
+            </button>
+            
+          </>
+        )}
+      </div>
+
+      {/* Render the pop-up conditionally */}
+      {isPopupOpen && (
+        <div className="popup fixed inset-0 flex items-center justify-center z-9999">
+          <div className='backdrop-blur-xl backdrop-brightness-75 w-10/12 h-3/6 bg-transparent rounded-xl'>
+
+            <h2>{slides[currentSlide].label}Pop-up Content</h2>
+            {/* Add your pop-up content here */}
+            
+            {/*if (condition) {                      {/* IT */}
+              
+            {/*}} else if (condition) {               {/* Photography */}
+              
+            {/*}} else {                              {/* Design */}
+
+            {/* */}
+            <button onClick={togglePopup}>Close</button>
+
+          </div>
+        </div>
+      )}
     </div>
   );
 };
