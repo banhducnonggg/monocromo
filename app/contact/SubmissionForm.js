57290967a7a4d187b761fb3afed1523bd292701a
banhducnonggg <banhducnonggg@gmail.com>
2023-10-11 18:55:20 -0600

    integration with vercel

--- app/contact/SubmissionForm.js
+++ app/contact/SubmissionForm.js
@@ -1,23 +1,13 @@
 import { useState } from 'react';
+import { useForm, ValidationError } from "@formspree/react";
 
 const SubmissionForm = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [category, setCategory] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // Do something with the form data
-    console.log('Name:', name);
-    console.log('Email:', email);
-    console.log('Category:', category);
-
-    // Reset form fields
-    setName('');
-    setEmail('');
-    setCategory('');
-  };
+  const [state, handleSubmit] = useForm("xbjvqgov");
+  const [formState, submit] = useForm("process.env.xjvqjyrb");
 
   return (
     <div className="flex items-center justify-center w-auto h-2/3">
@@ -30,21 +20,23 @@ const SubmissionForm = () => {
             name="name"
             placeholder='What should I call you?'
             value={name}
-            onChange={(e) => setName(e.target.value)}
+            // onChange={(e) => setName(e.target.value)}
             required
           />
+          <ValidationError prefix="Name" field="name" errors={state.errors} />
         </div>
         <div className="mb-4">
           <input
             className="inputForm text-md border-2 border-lColor bg-dColor rounded-xl px-3 py-2 w-full focus:drop-shadow-outGlow focus:border-lColor nocursor btn"
             type="email"
             id="email"
-            name='_replyto'
+            name='email'
             placeholder='Which email should I reply to?'
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            // onChange={(e) => setEmail(e.target.value)}
             required
           />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
         </div>
         <div className="mb-4">
           <select
@@ -60,13 +52,16 @@ const SubmissionForm = () => {
             <option className='text-lColor nocursor btn' value="Photography">Photography</option>
             <option className='text-lColor nocursor btn' value="Design">Design</option>
           </select>
+          <ValidationError prefix="Category" field="category" errors={state.errors} />
         </div>
         <button
           className="fixed submitButton text-md bg-lColor hover:bg-dColor text-dColor hover:text-lColor hover:drop-shadow-outGlow font-titleFont py-2 px-4 rounded-xl nocursor btn"
           type="submit"
+          disabled={state.submitting}
         >
           Let it goo ~
         </button>
+        <ValidationError errors={state.errors} />
       </form>
     </div>
   );
