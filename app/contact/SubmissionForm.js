c2dc327fbccd391874cb7b41602cef2ec0f0680c
banhducnonggg <banhducnonggg@gmail.com>
2023-10-11 18:40:20 -0600

    still fixing...

--- app/contact/SubmissionForm.js
+++ app/contact/SubmissionForm.js
@@ -1,41 +1,33 @@
-import React, { useState } from 'react';
-import { useForm, ValidationError } from '@formspree/react';
+import { useState } from 'react';
 
 const SubmissionForm = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [category, setCategory] = useState('');
-  const [state, handleSubmit] = useForm("mpzgkpdo");
 
-  if (state.succeeded) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Do something with the form data
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Category:', category);
+
+    // Reset form fields
     setName('');
     setEmail('');
     setCategory('');
-    return <p>Thanks for contacting!</p>;
-  }
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-
-  //   // Do something with the form data
-  //   console.log('Name:', name);
-  //   console.log('Email:', email);
-  //   console.log('Category:', category);
-
-  //   // Reset form fields
-  //   setName('');
-  //   setEmail('');
-  //   setCategory('');
-  // };
+  };
 
   return (
     <div className="flex items-center justify-center w-auto h-2/3">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} action="https://formspree.io/f/mpzgkpdo" method="POST">
         <div className="mb-4">
           <input
             className="inputForm text-md border-2 border-lColor bg-dColor rounded-xl px-3 py-2 w-full focus:drop-shadow-outGlow focus:border-lColor nocursor btn"
             type="text"
             id="name"
+            name="name"
             placeholder='What should I call you?'
             value={name}
             onChange={(e) => setName(e.target.value)}
@@ -47,6 +39,7 @@ const SubmissionForm = () => {
             className="inputForm text-md border-2 border-lColor bg-dColor rounded-xl px-3 py-2 w-full focus:drop-shadow-outGlow focus:border-lColor nocursor btn"
             type="email"
             id="email"
+            name='_replyto'
             placeholder='Which email should I reply to?'
             value={email}
             onChange={(e) => setEmail(e.target.value)}
@@ -57,6 +50,7 @@ const SubmissionForm = () => {
           <select
             className="inputForm text-md text-lColor border-2 border-lColor bg-dColor rounded-xl px-3 py-2 w-full focus:drop-shadow-outGlow focus:border-lColor nocursor btn"
             id="category"
+            name='category'
             value={category}
             onChange={(e) => setCategory(e.target.value)}
             required
